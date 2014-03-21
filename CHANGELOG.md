92f73d23836d617d43e80a1f97debde0534ffc5f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-03-21 15:40:41 -0300

    user agent

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.1
+
+- Request `User-Agent` is `'request'` by default
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = function (opts, cb) {
   if (typeof opts === 'string') {
   	opts = { url: opts.url };
   }
+  if (!opts.headers || !opts.headers['User-Agent']) {
+  	opts.headers['User-Agent'] = 'request';
+  }
   opts.error = cb;
 
   return reqwest(opts, function (res) {
